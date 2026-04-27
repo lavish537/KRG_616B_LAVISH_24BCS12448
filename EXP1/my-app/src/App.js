f883afc1d8683b41959affb5281e1ed8fb958da1
Lavish <24bcs12448@cuchd.in>
2026-04-27 17:02:33 +0530

    ADDED EXP1

---
+++ EXP1/my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>Counter</h2>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
